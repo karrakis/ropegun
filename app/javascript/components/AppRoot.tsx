341a72d859146a464b68c7ff503903e8cc3a7657
Maman Brigitte <karrakis@gmail.com>
2023-07-08 00:06:57 -0500

    still just playing

--- app/javascript/components/AppRoot.tsx
+++ app/javascript/components/AppRoot.tsx
@@ -6,58 +6,42 @@ export const AppRoot = () => {
       <div className="bg-blue-900 text-white h-8 w-full flex items-center sticky top-0">
         header
       </div>
-      <div className="bg-slate-300 bg-gradient-to-b from-blue-900">
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
-        <div className="text-red-500">Hi, all wired up.</div>
+      <div className="bg-slate-300 bg-gradient-to-b from-blue-900 flex flex-col">
+        <div className="w-full flex justify-left">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-end">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-left">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-end">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-left">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-center">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
+
+        <div className="w-full flex justify-end">
+          <div className="bg-yellow-300 bg-gradient-to-t from-slate-300 w-64 h-64"></div>
+        </div>
       </div>
     </>
   );
