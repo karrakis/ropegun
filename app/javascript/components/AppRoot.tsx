39b370f4222a4c5bb881ac53279c2f3e8a914274
Maman Brigitte <karrakis@gmail.com>
2023-07-08 00:01:42 -0500

    just playing around a bit

--- app/javascript/components/AppRoot.tsx
+++ app/javascript/components/AppRoot.tsx
@@ -1,12 +1,66 @@
 import React from "react";
 
 export const AppRoot = () => {
-    return (
+  return (
     <>
-    <div className="bg-red-500">wat</div>
-    <div className="text-red-500">Hi, all wired up.</div>
+      <div className="bg-blue-900 text-white h-8 w-full flex items-center sticky top-0">
+        header
+      </div>
+      <div className="bg-slate-300 bg-gradient-to-b from-blue-900">
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+        <div className="text-red-500">Hi, all wired up.</div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-export default AppRoot
+export default AppRoot;
