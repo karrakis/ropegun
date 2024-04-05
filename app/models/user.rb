07b7f6368676a4d87bf126a4281b3bbb77c3810d
Maman Brigitte <karrakis@gmail.com>
2024-04-04 19:01:16 -0500

    enum for quasibool values in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    
+    enum :top_rope_belay => [:no, :yes, :rusty], _prefix: :top_rope_belay
+    enum :lead_belay => [:no, :yes, :rusty], _prefix: :lead_belay
+    enum :trad_lead => [:no, :yes, :rusty], _prefix: :trad_lead
+    enum :multipitch => [:no, :yes, :rusty], _prefix: :multipitch
+    validates :email, presence: true
 end
