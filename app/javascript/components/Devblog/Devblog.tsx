be830d71cea187258dd57b1139c0960c2afb663a
Maman Brigitte <karrakis@gmail.com>
2024-05-28 16:38:13 -0500

    update devblog

--- app/javascript/components/Devblog/Devblog.tsx
+++ app/javascript/components/Devblog/Devblog.tsx
@@ -18,6 +18,10 @@ export const ComingSoon = () => {
           <li>Group Calendar for trips (when can people go?)</li>
           <li>Add ability to remove linked locations from account.</li>
           <li>Trip Plan location adds should add to list of saved locs.</li>
+          <li>Upload/change profile pictures</li>
+          <li>Upload/change climbing pictures</li>
+          <li>Link uploaded photos to trips, downloadable by participants.</li>
+          <li>Sort friends by skills and gear</li>
         </ul>
       </div>
     </div>
