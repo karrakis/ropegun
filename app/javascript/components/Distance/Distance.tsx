717393515c8020a14e7eb97e21b267f00a5f7be7
Maman Brigitte <karrakis@gmail.com>
2024-06-14 08:43:24 -0500

    average distances

--- app/javascript/components/Distance/Distance.tsx
+++ app/javascript/components/Distance/Distance.tsx
@@ -2,6 +2,164 @@ import React, { useState, useEffect } from "react";
 import { asynchronousStateUpdate } from "../../utilities/asynchronousStateUpdate";
 import { getDistances } from "../../utilities/getDistances";
 
+export const AverageDistances = ({ distances, guestDistances }) => {
+  console.log("**************************");
+  console.log(distances);
+  console.log(guestDistances);
+
+  let totalDistance = {};
+  let denominator = guestDistances.length + 1;
+
+  Object.keys(distances).map((destination) => {
+    if (distances[destination].distance) {
+      console.log(distances[destination].distance);
+      totalDistance[destination] ||= {};
+      totalDistance[destination].distance ||= {};
+      totalDistance[destination].distance.rows ||= [];
+      totalDistance[destination].distance.rows[0] ||= {};
+      totalDistance[destination].distance.rows[0].elements ||= [];
+      totalDistance[destination].distance.rows[0].elements[0] ||= {};
+      totalDistance[destination].distance.rows[0].elements[0].distance ||= {};
+      totalDistance[destination].distance.rows[0].elements[0].duration ||= {};
+      totalDistance[
+        destination
+      ].distance.rows[0].elements[0].distance.text ||= 0;
+      totalDistance[
+        destination
+      ].distance.rows[0].elements[0].duration.text ||= 0;
+      const miles = parseFloat(
+        distances[destination].distance.rows[0].elements[0].distance.text
+      );
+      const time = parseFloat(
+        distances[destination].distance.rows[0].elements[0].duration.value
+      );
+      totalDistance[destination].distance.rows[0].elements[0].distance.text +=
+        miles;
+      totalDistance[destination].distance.rows[0].elements[0].duration.text +=
+        time;
+    }
+  });
+
+  guestDistances.map((guest) => {
+    guest.distances.map((distance) => {
+      Object.keys(distance).map((destination) => {
+        console.log(
+          parseFloat(
+            distance[destination].distance.rows[0].elements[0].distance.text
+          )
+        );
+        console.log(
+          parseFloat(
+            distance[destination].distance.rows[0].elements[0].duration.value
+          )
+        );
+        if (distance[destination].distance) {
+          totalDistance[destination] ||= {};
+          totalDistance[destination].distance ||= {};
+          totalDistance[destination].distance.rows ||= [];
+          totalDistance[destination].distance.rows[0] ||= {};
+          totalDistance[destination].distance.rows[0].elements ||= [];
+          totalDistance[destination].distance.rows[0].elements[0] ||= {};
+          totalDistance[destination].distance.rows[0].elements[0].distance ||=
+            {};
+          totalDistance[destination].distance.rows[0].elements[0].duration ||=
+            {};
+          totalDistance[
+            destination
+          ].distance.rows[0].elements[0].distance.text ||= 0;
+          totalDistance[
+            destination
+          ].distance.rows[0].elements[0].duration.text ||= 0;
+
+          const miles = parseFloat(
+            distance[destination].distance.rows[0].elements[0].distance.text
+          );
+          const time = parseFloat(
+            distance[destination].distance.rows[0].elements[0].duration.value
+          );
+          totalDistance[
+            destination
+          ].distance.rows[0].elements[0].distance.text += miles;
+          totalDistance[
+            destination
+          ].distance.rows[0].elements[0].duration.text += time;
+        }
+      });
+    });
+  });
+
+  Object.keys(totalDistance).map((destination) => {
+    if (totalDistance[destination].distance) {
+      let distance = Math.floor(
+        totalDistance[destination].distance.rows[0].elements[0].distance.text /
+          denominator
+      );
+      let time =
+        totalDistance[destination].distance.rows[0].elements[0].duration.text /
+        denominator;
+      const hours = parseInt(time / 3600);
+      const minutes = (time / 60 - hours * 60).toFixed(0);
+
+      console.log(hours);
+      console.log(minutes);
+      totalDistance[
+        destination
+      ].distance.rows[0].elements[0].duration.text = `${hours} hours ${minutes} minutes`;
+      totalDistance[
+        destination
+      ].distance.rows[0].elements[0].distance.text = `${distance} mi`;
+    }
+  });
+
+  return displayDistances({ ...totalDistance });
+};
+
+const displayDistances = (distances) => {
+  console.log("distances:", distances);
+  return (
+    <table className="bg-night text-cream w-full">
+      <thead className="border border-auburn w-full">
+        <tr>
+          <th className="border-b border-l border-r border-auburn p-1">
+            Destination
+          </th>
+          <th className="border-b border-l border-r border-auburn p-1">
+            Miles
+          </th>
+          <th className="border-b border-l border-r border-auburn p-1">
+            Drive Time
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(distances).map((destination) => {
+          if (distances[destination].distance) {
+            return (
+              <tr key={`${destination}-distance`}>
+                <td className="border-b border-l border-r border-auburn p-1">
+                  {destination}
+                </td>
+                <td className="border-b border-l border-r border-auburn p-1">
+                  {
+                    distances[destination].distance.rows[0].elements[0].distance
+                      .text
+                  }
+                </td>
+                <td className="border-b border-l border-r border-auburn p-1">
+                  {
+                    distances[destination].distance.rows[0].elements[0].duration
+                      .text
+                  }
+                </td>
+              </tr>
+            );
+          }
+        })}
+      </tbody>
+    </table>
+  );
+};
+
 export const Distance = ({ locations, localUser, trip = nil }) => {
   const [distances, updateDistances] = useState({});
   const [guestDistances, updateGuestDistances] = useState([]);
@@ -72,61 +230,27 @@ export const Distance = ({ locations, localUser, trip = nil }) => {
     }
   }, [guestDistances]);
 
-  const displayDistances = (distances) => {
-    console.log("distances:", distances);
-    return (
-      <table className="bg-night text-cream w-full">
-        <thead className="border border-auburn w-full">
-          <tr>
-            <th className="border-b border-l border-r border-auburn p-1">
-              Destination
-            </th>
-            <th className="border-b border-l border-r border-auburn p-1">
-              Miles
-            </th>
-            <th className="border-b border-l border-r border-auburn p-1">
-              Drive Time
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(distances).map((destination) => {
-            if (distances[destination].distance) {
-              return (
-                <tr key={`${destination}-distance`}>
-                  <td className="border-b border-l border-r border-auburn p-1">
-                    {destination}
-                  </td>
-                  <td className="border-b border-l border-r border-auburn p-1">
-                    {
-                      distances[destination].distance.rows[0].elements[0]
-                        .distance.text
-                    }
-                  </td>
-                  <td className="border-b border-l border-r border-auburn p-1">
-                    {
-                      distances[destination].distance.rows[0].elements[0]
-                        .duration.text
-                    }
-                  </td>
-                </tr>
-              );
-            }
-          })}
-        </tbody>
-      </table>
-    );
-  };
-
   return (
     <div className="w-full">
       <h1 className="w-full text-cream bg-night text-xl p-2 text-center rounded-t-md">
-        Distances
+        Average Distances
+      </h1>
+      {distancesReady && (
+        <AverageDistances
+          distances={distances}
+          guestDistances={guestDistances}
+        />
+      )}
+      <h1 className="w-full text-cream bg-night text-xl p-2 text-center rounded-t-md">
+        Organizer Distances
       </h1>
       {distancesReady && displayDistances(distances)}
+      <h1 className="w-full text-cream bg-night text-xl p-2 text-center rounded-t-md mt-2">
+        Invitee Distances
+      </h1>
       {guestDistances.map((guest) => {
         return (
-          <div key={guest.label} className="w-full mt-2">
+          <div key={guest.label} className="w-full">
             <h2 className="w-full text-cream bg-night text-lg p-2 text-center rounded-t-md">
               {guest.label}
             </h2>
