42c02a6e726fd76b8ae8d0258c5643af10eecdad
Grubendol <karrakis@gmail.com>
2024-11-15 09:21:33 -0600

    pre-cleanup fix actual this time

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,16 +1,15 @@
-// esbuild.config.js
+const path = require('path')
 
-require('esbuild').build({
-    entryPoints: ['app/javascript/application.ts'],
-    outdir: 'app/assets/builds',
-    bundle: true,
-  
-    publicPath: '/assets',
-    assetNames: '[name]-[hash].digested',
-  
-    loader: {
-      '.svg': 'file',
-      '.png': 'file',
-      '.jpg': 'file',
-    }
-  })
+require("esbuild").build({
+  entryPoints: ["application.ts"],
+  bundle: true,
+  outdir: path.join(process.cwd(), "app/assets/builds"),
+  absWorkingDir: path.join(process.cwd(), "app/javascript"),
+  watch: process.argv.includes("--watch"),
+  loader: {
+    '.png': 'file',
+    '.svg': 'file',
+    '.jpg': 'file',
+    '.ico': 'file',
+  },
+}).catch(() => process.exit(1))
