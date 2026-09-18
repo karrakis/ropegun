28beeaf9d6348d87fef6e17d5134e2b09aca4b67
George Huestis <karrakis@gmail.com>
2026-09-18 14:59:17 -0500

    current_user should be a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
-    session[:userinfo]
+    @current_user ||= User.find_by(auth0_sub: session[:userinfo]["sub"]) if session[:userinfo]
   end
 
   def redirect_if_not_logged_in
