69411a71168574ae863f0f7096b3d22fdd7da647
George Huestis <karrakis@gmail.com>
2026-09-18 13:32:22 -0500

    styles for landing page

--- app/javascript/components/Home/Landing.tsx
+++ app/javascript/components/Home/Landing.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export const ComingSoon = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center my-16 min-h-screen w-full">
-      <div className="flex flex-col justify-center items-center w-full max-w-3xl bg-auburn text-cream p-4 rounded shadow-lg">
-        <h1 className="text-3xl font-bold mb-4">Going Somewhere?</h1>
+    <div className="w-full flex flex-col mt-8 items-center my-16 min-h-screen w-full">
+      <div className="flex flex-col justify-center items-center w-full max-w-3xl text-auburn p-4">
+        <h1 className="text-3xl font-bold">Going Somewhere?</h1>
       </div>
       <button className="flex flex-col justify-center items-center w-full max-w-3xl bg-auburn text-cream p-4 rounded shadow-lg mt-4">
-        <a href="/trip_plan">Get Going!</a>
+        <a href="/trip_plan" className="text-3xl">
+          Get Going!
+        </a>
       </button>
     </div>
   );
